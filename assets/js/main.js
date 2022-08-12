ddea3770ae39cfa030b6a4385550f40bd95115ff
Eve96-M <evemele45@gmail.com>
2022-08-12 15:14:11 -0430

    se agrego estructura HTML al main Evelio 12-08-22 3:45 Pm hora venezuela

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+
+document.addEventListener( "DOMContentLoaded", () =>{
+    load()
+})
+
+/* =========== LOADER ========== */
+const loader = document.getElementById( "loader" )
+function load () {
+    setTimeout(() => {
+        loader.classList.add( "hide" )
+    }, 3000);
+}
+
+/* =========DARK MODE======== */
+const themeButton = document.getElementById( "theme-button" )
+
+themeButton.addEventListener( "click", () =>{
+    document.body.classList.toggle( "dark-theme" )
+
+    if( themeButton.classList.contains( "bx-moon" ) ){
+        themeButton.classList.replace( "bx-moon", "bx-sun" )
+    }else{
+        themeButton.classList.replace( "bx-sun", "bx-moon" )
+    }
+})
+
+
+/*=======  CARRITO =========== */
+const cartOpen = document.getElementById( "cart-shop" )
+const cartClose = document.getElementById( "close-cart" )
+const cartContainer = document.getElementById( "cart-container" )
+
+cartOpen.addEventListener( "click", () => {
+    cartContainer.classList.remove( "hide" )
+})
+
+cartClose.addEventListener( "click", () => {
+    cartContainer.classList.add( "hide" )
+})
+
+
+
+/* ========SCROLL========= */
+const header = document.getElementById("header")
+
+window.addEventListener( "scroll", () =>{
+    if( window.scrollY >= 50 ){
+        header.classList.add("scroll-header")
+    }else{
+        header.classList.remove("scroll-header")
+    }
+})
+
